24352fb8a512fb98d500a1acd2d8f5241974bafd
Martin Schmidli <martin@flux.enterprises>
2021-05-29 23:21:04 +0200

    esbuild

--- craco.config.js
+++ craco.config.js
@@ -2,11 +2,12 @@
 /* https://ant.design/docs/react/use-with-create-react-app */
 
 const CracoLessPlugin = require('craco-less');
+const CracoEsbuildPlugin = require('craco-esbuild');
 
 module.exports = {
     plugins: [
         {
-            plugin: CracoLessPlugin,
+            plugin: CracoLessPlugin, CracoEsbuildPlugin,
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
