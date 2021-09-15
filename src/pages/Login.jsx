865896b3f7ff2e56d2d69105a36749ef71c6a8e1
Martin Schmidli <m.schmidli@smartit.ch>
2021-09-15 19:13:27 +0200

    small fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
         <div className="loginPage">
             <div className="loginCard">
                 <h1>Login</h1>
-                <p className="textCenter">Welcome to kiri.<br/>Use your AzureAD Accout to login.</p>
+                <p className="textCenter">Welcome to kiri.<br/>Use your Azure AD Accout to login.</p>
                 <SignInButton />
             </div>
         </div>
