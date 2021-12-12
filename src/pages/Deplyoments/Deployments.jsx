30e15f2737b346cbc1a55c8a51f50d4bc63b58d6
Martin Schmidli <m.schmidli@smartit.ch>
2021-12-12 22:01:31 +0100

    update

--- src/pages/Deplyoments/Deployments.jsx
+++ src/pages/Deplyoments/Deployments.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Switch, Button } from 'antd';
+import { Table, Button } from 'antd';
 import { Link } from "react-router-dom";
 import { renderDate } from 'util/renderDate';
 import moment from 'moment';
